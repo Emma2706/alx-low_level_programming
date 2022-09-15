6af60dec2466248ca0de6ca30347ddad48ea2faf
Emma2706 <emma.chisenwa@gmail.com>
2022-09-15 16:19:55 +0200

    C more functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase alphas
+ * @c: c is a variable
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (C >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0)
+}
